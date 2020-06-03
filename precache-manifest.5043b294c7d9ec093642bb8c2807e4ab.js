5c626475f5bf244bf493dc6985158bf2e88d5c29
theomgxz <sg28r8@gmail.com>
2020-06-02 20:55:44 -0400

    Updates

---
+++ precache-manifest.5043b294c7d9ec093642bb8c2807e4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17f51725c4d8c1e4d64447c46e61d5c",
+    "url": "/contactCards%20/index.html"
+  },
+  {
+    "revision": "062418992f1a06acd0f7",
+    "url": "/contactCards%20/static/css/main.bec292e6.chunk.css"
+  },
+  {
+    "revision": "f14f53966974888b859f",
+    "url": "/contactCards%20/static/js/2.05095c49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contactCards%20/static/js/2.05095c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062418992f1a06acd0f7",
+    "url": "/contactCards%20/static/js/main.2c91aa51.chunk.js"
+  },
+  {
+    "revision": "ff67806d89c00a0e2334",
+    "url": "/contactCards%20/static/js/runtime-main.10c9df59.js"
+  }
+]);
