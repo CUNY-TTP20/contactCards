0bafbce90c0901d48598d51e16daa31602fe473b
Sagar Shrestha <sg28r8@gmail.com>
2021-01-09 01:15:35 +0545

    Updates

---
+++ precache-manifest.8e514edc61ce547d7568e39798c6f1d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd4f1148ec5367781b472f76d06e058",
+    "url": "/contactCards/index.html"
+  },
+  {
+    "revision": "ea2cbf008303265b498e",
+    "url": "/contactCards/static/css/main.7a8eb324.chunk.css"
+  },
+  {
+    "revision": "f14f53966974888b859f",
+    "url": "/contactCards/static/js/2.05095c49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contactCards/static/js/2.05095c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2cbf008303265b498e",
+    "url": "/contactCards/static/js/main.ea42cd3b.chunk.js"
+  },
+  {
+    "revision": "5c3ebe9f3785d2e5ed8b",
+    "url": "/contactCards/static/js/runtime-main.a658a74b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contactCards%20/precache-manifest.077b668dfe87f6be486422a132e143dc.js"
+  "/contactCards/precache-manifest.8e514edc61ce547d7568e39798c6f1d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contactCards%20/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contactCards/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
